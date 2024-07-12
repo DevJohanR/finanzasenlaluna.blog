962ed19afa9ceffede76d2431b5215c4991f29c0
Johan Riascos <desarrollador7777@gmail.com>
2024-07-12 15:15:06 -0500

    Blog: deploy --produccion --testing

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,53 +1,54 @@
-import React, {useContext, useState} from 'react';
-import { Link } from 'react-router-dom'; // Asegúrate de importar Link de react-router-dom
+import React, { useContext, useState } from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../images/logo.png';
-import styles from './Header.module.css'; // Importando el CSS module
-import { FaBars } from 'react-icons/fa'
-import { AiOutlineClose } from 'react-icons/ai'
-
+import styles from './Header.module.css';
+import { FaBars } from 'react-icons/fa';
+import { AiOutlineClose } from 'react-icons/ai';
 import { UserContext } from '../../context/userContext';
 
 const Header = () => {
+  const [isNavShowing, setIsNavShowing] = useState(window.innerWidth > 800 ? true : false);
+  const { currentUser } = useContext(UserContext);
 
-  const [isNavShowing, setIsNavShowing] = useState(window.innerWidth > 800 ? true : false)
-  const {currentUser} = useContext(UserContext)
+  const toggleNav = () => {
+    setIsNavShowing(!isNavShowing);
+  };
 
-  const closeNavHandler = () =>{
-    if(window.innerWidth < 800){
+  const closeNavHandler = () => {
+    if (window.innerWidth < 800) {
       setIsNavShowing(false);
-    }else {
-      setIsNavShowing(true)
     }
-  }
+  };
 
   return (
     <nav>
       <div className={`${styles.navContainer} ${styles.container}`}>
         <Link to="/" className={styles.navLogo}>
-          <img src={Logo} style={{display:'block', objectFit:'cover', width:'100%'}} alt="Navbar Logo" />
+          <img src={Logo} alt="Navbar Logo" style={{ display: 'block', objectFit: 'cover', width: '100%' }} />
         </Link>
-       { currentUser?.id && isNavShowing && <ul className={styles.navMenu}>
-          <li><Link to={`/profile/${currentUser.id}`} onClick={closeNavHandler} >{currentUser?.name}</Link></li>
-          <li><Link to="/create" onClick={closeNavHandler}>Crear publicación</Link></li>
-          <li><Link to="/authors" onClick={closeNavHandler}>Autores</Link></li>
-          <li><Link to="/logout" onClick={closeNavHandler}>Cerrar sesión</Link></li>
-        </ul>} 
-        {!currentUser?.id && isNavShowing && <ul className={styles.navMenu}>
-          <li><Link to="/authors" onClick={closeNavHandler}>Autores</Link></li>
-          <li><Link to="/login" onClick={closeNavHandler}>Cerrar sesión</Link></li>
-        </ul>} 
-        <button className={styles.navToggleBtn}>
-        <AiOutlineClose/>
+        <button className={styles.navToggleBtn} onClick={toggleNav}>
+          {isNavShowing ? <AiOutlineClose /> : <FaBars />}
         </button>
+        {isNavShowing && (
+          <ul className={styles.navMenu}>
+            {currentUser?.id ? (
+              <>
+                <li><Link to={`/profile/${currentUser.id}`} onClick={closeNavHandler}>{currentUser.name}</Link></li>
+                <li><Link to="/create" onClick={closeNavHandler}>Crear publicación</Link></li>
+                <li><Link to="/authors" onClick={closeNavHandler}>Autores</Link></li>
+                <li><Link to="/logout" onClick={closeNavHandler}>Cerrar sesión</Link></li>
+              </>
+            ) : (
+              <>
+                <li><Link to="/authors" onClick={closeNavHandler}>Autores</Link></li>
+                <li><Link to="/login" onClick={closeNavHandler}>Iniciar sesión</Link></li>
+              </>
+            )}
+          </ul>
+        )}
       </div>
     </nav>
   );
-}
+};
 
 export default Header;
-
-
-
-
-
-
